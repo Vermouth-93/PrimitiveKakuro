a6ab5b499491c016fc0ae65b11bef6baae421329
Vermouth-93 <honeyantaka@yahoo.com>
2016-02-28 01:33:33 +0200

    Add new windows and buttons
    Create two other windows for winning and losing cases with more buttons
    for options of exiting game or playing again

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApplication1
 {
@@ -37,7 +25,7 @@ namespace WpfApplication1
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void button_Click_1(object sender, RoutedEventArgs e)
@@ -48,6 +36,45 @@ namespace WpfApplication1
         private void textBox1_TextChanged_1(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string box1, box2, box3, box4, box5, box6, box7, box8, box9;
+            box1 = text1.Text;
+            box2 = text2.Text;
+            box3 = text3.Text;
+            box4 = text4.Text;
+            box5 = text5.Text;
+            box6 = text6.Text;
+            box7 = text7.Text;
+            box8 = text8.Text;
+            box9 = text9.Text;
+            if (box1 == "6" && box2 == "1" && box3 == "9" &&
+                box4 == "2" && box5 == "6" && box6 == "4" &&
+                box4 == "2" && box5 == "6" && box6 == "4")
+            {
+                //correct solution
+                System.Console.WriteLine("Hello Correct");
+                System.Console.ReadLine();
+
+                PrimKakuro.Window1 newWin = new PrimKakuro.Window1();
+                newWin.Show();
+                //this.Close();
+            }
+            else
+            {
+                //wrong solution
+                System.Console.WriteLine("Hello Wrong");
+                System.Console.ReadLine();
+
+
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
